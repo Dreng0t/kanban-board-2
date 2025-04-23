33fbd312fb9f81ed8955f16a8113a931ae47a6eb
Dreng0t <louischicot4@gmail.com>
2025-04-23 15:41:49 +0100

    implemented a conditonal factor into the list

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -2,23 +2,34 @@ import kanban from '../data/kanban.json'
 import { useState } from "react";
 import ToDoList from './ToDoList';
 
-export default function ListItem(props){
+export default function ListItem(props) {
     console.log(props)
+
+    let priority;
+    if (props.todo.status === 'Done') {
+      priority = "✔️"
+    } else if (props.todo.status === 'In Progress') {
+      priority = '🙁'
+    } else {
+      priority =  '❌'
+    }
+
+
     return (
         <section className="ListItem">
-                    <ul key={props.todo.id} className="card">
-                        <li>
-                        <p>{props.todo.title}</p>
-                        <p>{props.todo.description}</p>
-                        <p>Assignee: {props.todo.assignee}</p>
-                        <p>Status: {props.todo.status}</p>
-                        <p>Added on {props.todo.createdDate}</p>
-                        <p>Due by {props.todo.dueDate}</p>
-                        <p>
-                            <button onClick={() => { props.functionToDelete(props.todo.id) }}>Delete this item</button>
-                        </p>
-                        </li>
-                    </ul>
+            <ul key={props.todo.id} className="card">
+                <li>
+                    <p>{props.todo.title}</p>
+                    <p>{props.todo.description}</p>
+                    <p>Assignee: {props.todo.assignee}</p>
+                    <p>Status: {props.todo.status} {priority}</p>
+                    <p>Added on {props.todo.createdDate}</p>
+                    <p>Due by {props.todo.dueDate}</p>
+                    <p>
+                        <button onClick={() => { props.functionToDelete(props.todo.id) }}>Delete this item</button>
+                    </p>
+                </li>
+            </ul>
         </section>
     )
 }
