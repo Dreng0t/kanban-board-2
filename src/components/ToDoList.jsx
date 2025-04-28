d1c67830dab544374342d76b56bab0ba7df9baee
Dreng0t <louischicot4@gmail.com>
2025-04-28 16:54:21 +0100

    implemented functionality to split the one list into 3 distinct lists based on the status of each item which are then displayed to the page side by side

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -4,26 +4,53 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function ToDoList(props) {
-    
+
+    const todoToDos = props.todosArr.filter((todo) => todo.status === "To Do");
+    const inProgressToDos = props.todosArr.filter((todo) => todo.status === "In Progress");
+    const doneToDos = props.todosArr.filter((todo) => todo.status === "Done");
+
     return (
         <section className="TodoList">
 
             <h1>Number of To Dos: {props.todosArr.length}</h1>
 
             <Link to="/create">
-            <button className="btn btn-primary">Add To Do</button>
+                <button className="btn btn-primary">Add To Do</button>
             </Link>
 
-            {props.todosArr.map((todoObj) => {
-                console.log(todoObj);
-                return (
-                    <ul key={todoObj.id} className="card">
-                        <li>
-                        <ListItem todo={todoObj} deleteTodo={props.deleteTodo}/>
-                        </li>
-                    </ul>
-                )
-            })}
+
+            <div className="board">
+                <div className="column">
+                    <h2>To Do</h2>
+                    {todoToDos.map((todoObj) => (
+                        <ul key={todoObj.id} className="card">
+                            <li>
+                                <ListItem todo={todoObj} deleteTodo={props.deleteTodo} />
+                            </li>
+                        </ul>
+                    ))}
+                </div>
+                <div className="column">
+                    <h2>In Progress</h2>
+                    {inProgressToDos.map((todoObj) => (
+                        <ul key={todoObj.id} className="card">
+                            <li>
+                                <ListItem todo={todoObj} deleteTodo={props.deleteTodo} />
+                            </li>
+                        </ul>
+                    ))}
+                </div>
+                <div className="column">
+                    <h2>Done</h2>
+                    {doneToDos.map((todoObj) => (
+                        <ul key={todoObj.id} className="card">
+                            <li>
+                                <ListItem todo={todoObj} deleteTodo={props.deleteTodo} />
+                            </li>
+                        </ul>
+                    ))}
+                </div>
+            </div>
 
         </section>
     )
